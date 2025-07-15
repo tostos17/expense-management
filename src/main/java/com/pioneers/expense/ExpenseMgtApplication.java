1af57e7e68013b955e60784ed14cc45a237ef213
Oluwatosin Olufowobi <fowobideveloper@gmail.com>
2025-07-15 06:18:00 +0100

    initial commit

---
+++ src/main/java/com/pioneers/expense/ExpenseMgtApplication.java
@@ -0,0 +1,13 @@
+package com.pioneers.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pioneers/expense/ExpenseMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioneers.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
